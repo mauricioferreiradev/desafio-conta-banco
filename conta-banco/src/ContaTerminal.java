0699c1c69b53cd2c4d183551a5f3e999850a9a81
Mauricio Ferreira <mauricioferreira.dev@hotmail.com>
2024-05-19 16:21:29 -0300

    importanto a classe scanner e escrevendo a interação com o usuario

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //TO DO: conhecer e importar a classe scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados pelo usuário
+        //Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número do seu banco:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da sua Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
     }
 }
